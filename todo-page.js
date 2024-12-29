f34f310634ec5b4a5b99c83d3a0054e761e028fa
Alexandra <alexandraghiorghita88@gmail.com>
2024-12-29 12:42:15 +0200

    Add tasks in catgeories + sync the completion of a task in the category and main

--- todo-page.js
+++ todo-page.js
@@ -21,6 +21,116 @@ const categoryList = ["work", "home", "health", "social", "perosnal"];
 
 let todoList = [];
 
+let idTask = 0;
+
+let taskElements = {};
+
+const syncCheckboxState = (taskId, completed) => {
+    const taskRefs = taskElements[taskId];
+
+    if (taskRefs) {
+        const mainCheckbox = taskRefs.main;
+        const categoryCheckbox = taskRefs.category;
+
+        if (completed === "yes") {
+            mainCheckbox.classList.add("checked");
+            mainCheckbox.classList.remove("unchecked");
+
+            categoryCheckbox.classList.add("checked");
+            categoryCheckbox.classList.remove("unchecked");
+        } else {
+            mainCheckbox.classList.add("unchecked");
+            mainCheckbox.classList.remove("checked");
+
+            categoryCheckbox.classList.add("unchecked");
+            categoryCheckbox.classList.remove("checked");
+        }
+    }
+}
+
+const addTaskInMain = todo => {
+    const taskToAdd = document.createElement("div");
+    taskToAdd.className = "task-entry-main";
+    taskToAdd.dataset.id = todo.id;
+
+    const taskTitle = document.createElement("p");
+    taskTitle.className = "task-title-main";
+    taskTitle.textContent = todo.title;
+    taskToAdd.appendChild(taskTitle);
+
+    const taskDate = document.createElement("p");
+    taskDate.className = "task-date-main";
+    taskDate.innerHTML = `${todo.date} <i class="fas fa-calendar-alt"></i>`;
+    taskToAdd.appendChild(taskDate);
+
+    const taskCheckbox = document.createElement("div");
+    taskCheckbox.className = "task-checkbox-main unchecked";
+
+    taskCheckbox.addEventListener("click", () => {
+        todo.completed = todo.completed === "yes" ? "no" : "yes";
+        syncCheckboxState(todo.id, todo.completed);
+    });
+
+    taskToAdd.appendChild(taskCheckbox);
+
+    const taskDescription = document.createElement("p");
+    taskDescription.className = "task-description-main";
+    taskDescription.textContent = todo.description;
+    taskToAdd.appendChild(taskDescription);
+
+    mainTasksList.appendChild(taskToAdd);
+
+    taskElements[todo.id].main = taskCheckbox;
+}
+
+const addTaskInCategory = todo => {
+
+    const taskToAdd = document.createElement("div");
+    taskToAdd.className = "task-entry-category";
+    taskToAdd.dataset.id = todo.id;
+
+    const taskTitle = document.createElement("p");
+    taskTitle.className = "task-title-category";
+    taskTitle.textContent = todo.title;
+    taskToAdd.appendChild(taskTitle);
+
+    const taskDate = document.createElement("p");
+    taskDate.className = "task-date-category";
+    taskDate.innerHTML = `${todo.date} <i class="fas fa-calendar-alt"></i>`;
+    taskToAdd.appendChild(taskDate);
+
+    const taskCheckbox = document.createElement("div");
+    taskCheckbox.className = "task-checkbox-category unchecked";
+
+    taskCheckbox.addEventListener("click", () => {
+        todo.completed = todo.completed === "yes" ? "no" : "yes";
+        syncCheckboxState(todo.id, todo.completed);
+    });
+
+    taskToAdd.appendChild(taskCheckbox);
+
+    switch(todo.category) {
+        case "work":
+            workTasksList.appendChild(taskToAdd);
+            break;
+        case "home":
+            householdTasksList.appendChild(taskToAdd);
+            break;
+        case "health":
+            healthTasksList.appendChild(taskToAdd);
+            break;
+        case "social":
+            socialTasksList.appendChild(taskToAdd);
+            break;
+        case "personal":
+            personalDevelopmentTasksList.appendChild(taskToAdd);
+            break;
+        default: break;
+    }
+
+    taskElements[todo.id].category = taskCheckbox;
+}
+
 todoForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
@@ -34,7 +144,8 @@ todoForm.addEventListener("submit", (event) => {
         description: description,
         category: category,
         date: date,
-        completed: "no"
+        completed: "no",
+        id: idTask
     }
 
     todoList.push(todo);
@@ -44,15 +155,12 @@ todoForm.addEventListener("submit", (event) => {
     todoCategory.value = "";
     todoDate.value = "";
 
-    const taskToAdd = 
-    `<div class="task-entry-main">
-        <p class="task-title-main">${todo.title}</p>
-        <p class="task-date-main">${todo.date} <i class="fas fa-calendar-alt"></i></p>
-        <div class="task-checkbox-main unchecked"></div>
-        <p class="task-description-main">${todo.description}</p>
-    </div>`
+    taskElements[todo.id] = {};
 
-    mainTasksList.innerHTML += taskToAdd;
+    addTaskInMain(todo);
+
+    addTaskInCategory(todo);
 
     console.log(todoList);
+    console.log(taskElements);
 });
